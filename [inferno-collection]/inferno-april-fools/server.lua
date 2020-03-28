e54e028fdd9b0c3760a0bfc72dc5af146249d7fc
Daniel Hawton <daniel@hawton.org>
2020-03-28 15:40:25 -0500

    tweak logic to not rely on clients, and allow auto fool

--- [inferno-collection]/inferno-april-fools/server.lua
+++ [inferno-collection]/inferno-april-fools/server.lua
@@ -1,6 +1,7 @@
 -- Inferno Collection April Fools Version 1.0 Alpha
 --
 -- Copyright (c) 2019, Christopher M, Inferno Collection. All rights reserved.
+-- Copyright (c) 2020, Daniel A. Hawton, Inferno Collection
 --
 -- This project is licensed under the following:
 -- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 'Software'), to use, copy, modify, and merge the software, under the following conditions:
@@ -9,29 +10,43 @@
 --
 
 --
---		Nothing past this point needs to be edited, all the settings for the resource are found ABOVE this line.
+--		Nothing past this point needs to be edited, all the settings for the resource are found in the config.lua file.
 --		Do not make changes below this line unless you know what you are doing!
 --
 
 local AllPlayers = {}
+local players = {}
 
-RegisterServerEvent('April-Fools:Register')
-AddEventHandler('April-Fools:Register', function()
-    if not AllPlayers[source] then
-        AllPlayers[source] = true
-    end
-end)
-
-AddEventHandler('playerDropped', function ()
-    if AllPlayers[source] then
-        AllPlayers[source] = nil
-    end
-end)
-
-RegisterCommand('kickall', function(source)
-    if source == 0 then
-        for ID, _ in pairs(AllPlayers) do
-            DropPlayer(ID, '\n\nYou have been permanently banned from this server, reason:\n\nView reason here: https://bit.ly/2y6Onlx')
+if Config.onConnect then
+    AddEventHandler("playerConnecting", function()
+        local t = os.date("*t")
+        if t.month == Config.onConnectDate.month and t.day == Config.onConnectDate.day then
+            deferrals.defer()
+            if not players[source] then
+                players[source] = true   -- They can reconnect
+                CancelEvent()
+                deferrals.done("You have been permanently banned from this server. You can view the reason and appeal this ban at " + Config.url)
+            end
         end
-    end
-end)
+    end)
+end
+
+if Config.registerCommand then
+    AddEventHandler("playerSpawned", function()
+        if not AllPlayers[source] then
+            AllPlayers[source] = true
+        end
+    end)
+
+    AddEventHandler("playerDropped", function()
+        AllPlayers[source] = nil
+    end)
+
+    RegisterCommand(Config.commandName, function(source)
+        if source == 0 then
+            for ID, _ in pairs(AllPlayers) do
+                DropPlayer(ID, '\n\nYou have been permanently banned from this server, reason:\n\nView reason and appeal at: ' + Config.url)
+            end
+        end
+    end)
+end
